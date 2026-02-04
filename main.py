4c53a8357b0522c9d2701453ad296fcd5e35c2cf
quanghunghhcc <quanghunghhcc@gmail.com>
2026-02-04 23:39:06 +0700

    súc nô

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from src.data_loader import data_load
 # Test thử
 def main(): 
     df = data_load('data/raw/movies.csv')
-
+    
     popular_model = PopularRecommender(df)
     res = popular_model.top_popular(df)
 
     print(res)
     res.to_csv('data/raw/example.csv')
-    
+    print("dm chung")
 
 
 
