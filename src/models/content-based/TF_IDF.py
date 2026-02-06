6b4ed963dec7ab2ab08092dc7967a030113c012d
chungnv05 <chungnv05.stu@gmail.com>
2026-02-06 18:08:25 +0700

    add build_vocab

--- src/models/content-based/TF_IDF.py
+++ src/models/content-based/TF_IDF.py
@@ -6,15 +6,22 @@ import math
 
 
 
-def TfVectorizer(df: pd.DataFrame) -> list: # tính vector tf cho từng phim
-    raw_genres = df['genres'].to_list()
+def build_vocab(df: pd.DataFrame, col_name: str):
+    raw_genres = df[col_name].to_list()
 
-    vocab = sorted(set(g.lower() for movie in raw_genres for g in movie))
-    word_idx = {g:i for i,g in enumerate(vocab)}
+    docs = [[g.lower().strip() for g in movie] for movie in raw_genres]
+    vocab = sorted(set(g for movie in docs for g in movie))
+    word_idx = {g:i for i, g in enumerate(vocab)}
+
+    return docs, vocab, word_idx
+
+def TfVectorizer(df: pd.DataFrame) -> list: # tính vector tf cho từng phim
+    
+    docs,vocab, word_idx = build_vocab(df,col_name= 'soup')
 
     tf_vectors = []
 
-    for movie in raw_genres:
+    for movie in docs:
         counter = Counter(movie)
         vector = [0]*len(vocab)
         for g, count in counter.items():
@@ -24,17 +31,15 @@ def TfVectorizer(df: pd.DataFrame) -> list: # tính vector tf cho từng phim
     return tf_vectors
 
 def IdfVectorizer(df: pd.DataFrame) -> list: # tính vector idf
-    raw_genres = df['genres'].to_list()
-
-    vocab = sorted(set(g.lower() for movie in raw_genres for g in movie))
+    docs, vocab, word_idx = build_vocab(df,col_name='soup')
     idf_vector = [0]*len(vocab)
-    word_idx = {g:i for i,g in enumerate(vocab)}
+    word_set = [set(movie) for movie in docs]
     for word in vocab:
         freq = 0
-        for movie in raw_genres:
-            if word in set(movie):
+        for movie in word_set:
+            if word in movie:
                 freq += 1
-        idf_vector[word_idx[word]] = math.log((len(raw_genres) + 1)/(freq + 1)) + 1
+        idf_vector[word_idx[word]] = math.log((len(docs) + 1)/(freq + 1)) + 1
     
     return idf_vector
         
